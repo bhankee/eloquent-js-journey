9b6d3d1f42935a2caaeefa8993bb5e2d5e6dccbe
Brad Hankee <brad.hankee@gmail.com>
2018-10-01 07:27:25 -0400

    Finished ch.4.

--- chapter-4/README.md
+++ chapter-4/README.md
@@ -1,3 +1,3 @@
 # Chapter Four Problems
 
-solved:
+solved:✅
--- chapter-4/ch4.js
+++ chapter-4/ch4.js
@@ -19,6 +19,30 @@ console.log(sum(range(1, 5)));
 // Reversing an Array = without .reverse().
 function reverseArray(arr) {
   let arrCopy = [];
-  console.log(arr.shift());
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arrCopy.push(arr[i]);
+  }
+  console.log(arrCopy);
 }
 reverseArray([1, 3, 5]);
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let org = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = org;
+  }
+  return array;
+}
+
+reverseArrayInPlace([11, 2, 30]);
+
+// Array to List
+function arrayToList(arr) {
+  let result = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    result = { value: arr[i], rest: result };
+  }
+  return result;
+}
